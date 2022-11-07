d2b300fd4bdf06a137c2f2767d3a3881cf908fb8
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-11-07 12:01:45 -0300

    2.Sintaxis, variables y palabras reservadas

---
+++ 2.Sintaxis/lista.js
@@ -0,0 +1,14 @@
+const mis_datos = [
+    "Oscar",
+    40,
+    true,
+    new Date(1982,6,20),
+    {
+      Titulo: "EL SEÑOR DE LOS ANILLOS 1. La comunidad del anillo (TV Tie-In) - THE LORD OF THE RINGS 1. The Fellowship of the Ring (TV Tie-In) (Spanish edition)",
+      Autor: "J.R.R. Tolkien",
+      fecha: new Date(1954,7,29),
+      url: "https://www.amazon.com/-/es/J-R-R-Tolkien/dp/6070792238/ref=sr_1_1?crid=3RD8P80R4DJZC&keywords=el+se%C3%B1or+de+los+anillos+libros+en+espa%C3%B1ol&qid=1667832715&sprefix=libros+en+espa%C3%B1ol+se%C3%B1or+de+%2Caps%2C271&sr=8-1"
+    },
+];
+
+
