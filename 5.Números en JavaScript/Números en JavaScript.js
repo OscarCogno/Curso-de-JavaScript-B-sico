a4d23f0018756a7a252cad9263c48a0f25dc456b
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-11-14 16:12:27 -0300

    5.Números en JavaScript

---
+++ 5.Números en JavaScript/Números en JavaScript.js
@@ -0,0 +1,13 @@
+let alturaCm = 185;
+let alturaM = 1.85;
+let pesoKg = 108.7;
+let alturaMRA = Math.round(alturaM);
+let pesoKgRAA = Math.floor(pesoKg);
+let maximoValor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(alturaCm);
+console.log(alturaM);
+console.log(pesoKg);
+console.log(alturaMRA);
+console.log(pesoKgRAA);
+console.log(maximoValor);
