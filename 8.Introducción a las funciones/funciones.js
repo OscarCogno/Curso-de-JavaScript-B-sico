ecfa3612542dbafe184e67116c575d25c2b1b63b
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-11-25 11:35:34 -0300

    8.Introducción a las funciones

---
+++ 8.Introducción a las funciones/funciones.js
@@ -0,0 +1,24 @@
+function sin_parametros(){
+    return true
+}
+
+async function promesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"),5000)
+}
+
+function* generapares(){
+    let id = 0;
+    while(true){
+        yield id +=2
+    }
+}
+
+const sp = sin_parametros()
+console.log(sp)
+
+const gene = generapares()
+console.log(gene.next().value)
+console.log(gene.next().value)
+console.log(gene.next().value)
+console.log(gene.next().value)
+ 
