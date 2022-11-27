c9f9ee77048a08cd86c6d99e03e795f8701725f2
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-11-27 08:04:36 -0300

    9.Gestión de errores

---
+++ 9.Gestión-de-errores/index.js
@@ -0,0 +1,28 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+logger.error("Esto es un error")
+
+function miError(){
+  throw new Error("Error");
+}
+
+try {
+  miError();
+}
+catch(e){
+console.error("ERROR")
+}
